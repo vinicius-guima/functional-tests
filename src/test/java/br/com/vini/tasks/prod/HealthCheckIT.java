58922074e9bc145e46a2b924fd9f8c28932e1511
vinicius-guima <vinicius_guima.social@hotmail.com>
2021-04-21 14:30:29 -0300

    add health check test

--- src/test/java/br/com/vini/tasks/prod/HealthCheckIT.java
+++ src/test/java/br/com/vini/tasks/prod/HealthCheckIT.java
@@ -19,7 +19,7 @@ public class HealthCheckIT {
 		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 		WebDriver driver = new RemoteWebDriver(new URL("http://172.20.0.1:4444/wd/hub"), capabilities);
 		try {
-			driver.navigate().to("http://127.0.0.1:9999/tasks/");
+			driver.navigate().to("http://192.168.0.24:9999/tasks/");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 			String text = driver.findElement(By.id("version")).getText();
