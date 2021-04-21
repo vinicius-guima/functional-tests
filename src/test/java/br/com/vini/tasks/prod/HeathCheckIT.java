06407dc7af9786ffba847751ef39aa2ac9588d3e
vinicius-guima <vinicius_guima.social@hotmail.com>
2021-04-21 14:09:42 -0300

    add health check test

---
+++ src/test/java/br/com/vini/tasks/prod/HeathCheckIT.java
@@ -0,0 +1,35 @@
+package br.com.vini.tasks.prod;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import junit.framework.Assert;
+
+public class HeathCheckIT {
+
+	@Test
+	public void heathCheck() throws MalformedURLException {
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://172.20.0.1:4444/wd/hub"), capabilities);
+		try {
+			driver.navigate().to("http://192.168.0.24:9999/tasks/");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+			String text = driver.findElement(By.id("version")).getText();
+			System.out.println(text);
+			assertTrue(text.contains("build"));
+		} finally {
+			driver.quit();
+		}
+	}
+}
