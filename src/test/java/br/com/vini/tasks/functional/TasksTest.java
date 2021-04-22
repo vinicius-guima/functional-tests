aaa8373cebd55a6540c58fa8abaebe9b7ddec888
vinicius-guima <vinicius_guima.social@hotmail.com>
2021-04-21 21:40:56 -0300

    teste de remoção

--- src/test/java/br/com/vini/tasks/functional/TasksTest.java
+++ src/test/java/br/com/vini/tasks/functional/TasksTest.java
@@ -96,5 +96,24 @@ public class TasksTest {
 			driver.quit();
 		}
 	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+		WebDriver driver = acessarApp();
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("inserção e remoção");
+			driver.findElement(By.id("dueDate")).sendKeys("15/02/2030");
+			driver.findElement(By.id("saveButton")).click();
+			String text = driver.findElement(By.id("message")).getText();
+			assertEquals("Success!", text);
+			
+			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+			text = driver.findElement(By.id("message")).getText();
+			assertEquals("Success!", text);
+		} finally {
+			driver.quit();
+		}
+	}
 
 }
